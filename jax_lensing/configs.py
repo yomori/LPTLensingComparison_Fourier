859dc584fa5363f4e1f3030a45a980c6a83b5bfd
yomori <yuuki.om@gmail.com>
2024-02-19 12:19:24 -0800

    added DES-Y3 config

--- jax_lensing/configs.py
+++ jax_lensing/configs.py
@@ -74,3 +74,27 @@ LSST_Y10 = config(
     sigma_e=0.26,
     ell_max=3000
 )
+
+# DES Y3 configuration
+# n(z) extracted from 2pt_NG_final_2ptunblind_02_26_21_wnz_maglim_covupdate.fits
+# Number density extracted from Table I of 2105.13541
+# Assuming constant sig_e taking straight-up average 0.267=(0.243+0.262+0.259+0.301)/4 
+nzs = np.load('nz/nz_desy3_metacal.npy') 
+DES_Y3 = config(
+    box_shape  = [300, 300, 128],
+    box_size   = [400., 400., 4000.],
+    field_size = 5.0,
+    field_npix = 60,
+    fiducial_cosmology=jc.Planck15(Omega_c=0.2664, Omega_b=0.0492, sigma8=0.831, h=0.6727, n_s=0.9645),
+    priors={
+            "Omega_c": dist.Normal(0.2664, 0.2),
+            "Omega_b": dist.Normal(0.0492, 0.006),
+            "sigma8" : dist.Normal(0.831, 0.14),
+            "h"      : dist.Normal(0.6727, 0.063),
+            "n_s"    : dist.Normal(0.9645, 0.08),
+            "w0"     : dist.Normal(-1.0, 0.8),
+           },
+    nz_shear=[jc.redshift.kde_nz(nzs[:,0], nzs[:,zi+1], bw=0.01, zmax=3.0, gals_per_arcmin2=g ) for zi, g in enumerate([1.476,1.479,1.484,1.461])],
+    sigma_e=0.267,
+    ell_max=3000
+)
